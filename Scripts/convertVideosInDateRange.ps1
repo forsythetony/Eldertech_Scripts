26c507a375b206f2908fad8186fd6155a2d922b4
forsythetony <forsythetony@gmail.com>
2014-05-21 21:57:08 -0500

    Fixes range selector

--- Scripts/convertVideosInDateRange.ps1
+++ Scripts/convertVideosInDateRange.ps1
@@ -4,9 +4,9 @@ function getDates
 {
     #Prompt the user to enter the date range
 
-    $startDate = Read-Host "Enter the start date for the date range in the format M/d/YYYY (ex. 4/22/2014)"
+    $startDate = Read-Host "Enter the start date for the date range in the format M/d/YYYY"
     
-    $endDate = Read-Host "Enter the end date for the date range in the format M/d/YYYY (ex. 4/22/2014)"
+    $endDate = Read-Host "Enter the end date for the date range in the format M/d/YYYY"
 
     $startDate = [dateTime]::ParseExact($startDate, "M/d/yyyy", $null)
     $endDate = [dateTime]::ParseExact($endDate, "M/d/yyyy", $null)
@@ -20,21 +20,26 @@ function getDates
 }
 function updateFilesInRange($range, $pathToFiles)
 {
+
     # Selection all items within the $pathToFiles directory that meet the following conditions...
     # 1. It is not a directory
     # 2. It was created after the start of the date range
     # 3. It was created before the end of the date range
-    Get-ChildItem -Path $pathToFiles -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.Name -like "*.avi" -and  $_.CreationTime -gt $range.start -and $_.CreationTime -lt $range.end} | Foreach-Object{
+    Get-ChildItem -Path $pathToFiles -Recurse | Where-Object {$_.Name -like "*.txt" -and !$_.PSIsDirectory -and $_.CreationTime -gt $range.start -and $_.CreationTime -lt $range.end} | Foreach-Object{
        
         $filePath = $_.FullName
         
+        Write-Host $filePath
         # Here you would use the file path along with ffmpeg to do the conversion
         
    }
 }
 # Main program
 
+$pathToFolder = "$env:userprofile\Desktop\testDirectory\"
+
+# Write-Host $pathToFolder
+
 $dates = getDates
 
-Write-Host ("The start date is " + ($dates.start))
-Write-Host ("The end date is " + ($dates.end))
+updateFilesInRange $dates $pathToFolder
