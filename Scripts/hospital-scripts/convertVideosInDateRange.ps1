9506fbef192de9c8f42f95becbe26da6a46977a4
forsythetony <forsythetony@gmail.com>
2014-06-03 12:56:29 -0700

    Updates video conversion script

--- Scripts/hospital-scripts/convertVideosInDateRange.ps1
+++ Scripts/hospital-scripts/convertVideosInDateRange.ps1
@@ -1,45 +1,218 @@
 ﻿# Function definitions
-
-function getDates
+ 
+function getUserData
 {
-    #Prompt the user to enter the date range
-
-    $startDate = Read-Host "Enter the start date for the date range in the format M/d/YYYY"
-    
-    $endDate = Read-Host "Enter the end date for the date range in the format M/d/YYYY"
-
-    $startDate = [dateTime]::ParseExact($startDate, "M/d/yyyy", $null)
-    $endDate = [dateTime]::ParseExact($endDate, "M/d/yyyy", $null)
-
-    $dateRange = @{"start" = $startDate;
-                   "end" = $endDate;
-                   }
-
-    return $dateRange;
-                    
-}
-function updateFilesInRange($range, $pathToFiles)
-{
-
-    # Selection all items within the $pathToFiles directory that meet the following conditions...
-    # 1. It is not a directory
-    # 2. It was created after the start of the date range
-    # 3. It was created before the end of the date range
-    Get-ChildItem -Path $pathToFiles -Recurse | Where-Object {$_.Name -like "*.avi" -and !$_.PSIsDirectory -and $_.CreationTime -gt $range.start -and $_.CreationTime -lt $range.end} | Foreach-Object{
+    Do {
+        $rootPath = Read-Host "Enter the path to the folder containing video files (NOT /userid)"
+        # $rootPath = "C:\Users\muengrcerthospkinect\Desktop\testing"
+ 
+        $pathTest = Test-Path $rootPath
+ 
+        if (!$pathTest) { Write-Host "Path provided was not valid, try again." }
+        } while ($pathTest -eq $false)
+ 
        
-        $filePath = $_.FullName
+ 
+    Do {
+        $userID = Read-Host "Enter the userID (Enter all to search all files)"
+ 
+        $isValidID = checkUserIDString $userID
+       
         
-        Write-Host $filePath
-        # Here you would use the file path along with ffmpeg to do the conversion
-        
-   }
+        Write-Host $isValidID.message
+ 
+        $userID = $isvalidID.path
+ 
+        } while ($isValidID.isValid -eq $false)
+ 
+    $rootPathLength = $rootPath.length - 1
+ 
+    if($rootPath[$rootPathLength] -ne "\")
+    {
+        $rootPath = "$rootPath\"
+    }
+ 
+    $folderPath = ($rootPath + $userID);
+ 
+ 
+    $startDate = Read-Host "Enter the start date for the date range in the format M/d/YYYY h:m AM/PM"
+    # $startDate = "5/20/2013"
+    $endDate = Read-Host "Enter the end date for the date range in the format M/d/YYYY h:m AM/PM"
+    # $endDate = "5/20/2013"
+    $startParseString = checkDateString $startDate
+    $endParseString = checkDateString $endDate
+   
+    $startDate = [dateTime]::ParseExact($startDate, $startParseString, $null)
+    $endDate = [dateTime]::ParseExact($endDate, $endParseString , $null)
+ 
+ 
+    $userData = @{
+                    "start" = $startDate;
+                    "end" = $endDate;
+                    "folderPath" = $folderPath;
+                 }
+ 
+    return $userData
 }
+function checkUserIDString($string)
+{
+    $length = $string.length
+ 
+    if ($string -eq "all" -or $string -eq "All")
+    {
+        $package = @{ "isValid" = $true;
+                    "path" = "";
+                    "message" = "User has chosen to search all user IDs";
+                   }
+        return $package
+    }
+ 
+ 
+    if ($length -ne 3)
+    {
+        
+       $package = @{ "isValid" = $false;
+                    "path" = $null;
+                    "message" = "The user ID entered is not of the right length. Please try again.";
+                   }
+ 
+        return $package
+ 
+    }
+   
+    if (!($string -match "^[0-9]*$"))
+    {
+    
+        $package = @{ "isValid" = $false;
+                    "path" = $null;
+                    "message" = "The user ID entered is not a numeric value. Please try again.";
+                   }
+ 
+        return $package
+    
+    }
+ 
+    $package = @{ "isValid" = $true;
+                    "path" = $string;
+                    "message" = "The userID entered was in the correct format.";
+                   }
+ 
+    return $package
+}
+function checkDateString($string)
+{
+    $tokens = $string.Split(" ")
+ 
+    $count = $tokens.length
+   
+    if ($count -eq 1)
+    {
+        $parseString = "M/d/yyyy"
+    }
+    elseif ($count -eq 2)
+    {
+        $parseString = "M/d/yyyy H:m"
+    }
+    elseif ($count -eq 3)
+    {
+        $parseString = "M/d/yyyy h:m tt"
+    }
+ 
+    return $parseString
+ 
+}
+function updateFilesInRange($range)
+{
+    $pathToFiles = "\\echo\mcp\100\"
+   
+    $theChild = Get-ChildItem -Path $pathToFiles | Where {$_.PSIsContainer -eq $true -and $_.Name -eq "KinectData"}
+ 
+    Get-ChildItem -Path $theChild.FullName | Where {$_.PSIsContainer -eq $true} | Foreach {
+ 
+        $folderDate = extractDateFromFolder $_.Name
+ 
+        if ($folderDate -ne $null)
+        {
+            Write-Host $folderDate
+ 
+        }
+    }
+ 
+}
+function updateFilesTest
+{
+ $pathToFiles = "\\echo\mcp\100\"
+   
+
+   # $dateRangeOne = @{ "startDate" : [dateTime]::ParseExact("6/1/2012", "M/d, $null)
+    $theChild = Get-ChildItem -Path $pathToFiles | Where {$_.PSIsContainer -eq $true -and $_.Name -eq "KinectData"}
+ 
+    Get-ChildItem -Path $theChild.FullName | Where {$_.PSIsContainer -eq $true} | Foreach {
+ 
+        $folderDate = extractDateFromFolder $_.Name
+ 
+        if ($folderDate -ne $null -and $folderDate)
+        {
+            Write-Host $folderDate
+ 
+        }
+    }
+
+
+
+}
+function getRanges($rangeOption)
+{
+    switch ($rangeOption)
+    {
+        1 {
+            Write-Host "The user chose range option 1"
+        }
+
+        
+
+
+
+
+    }
+
+
+
+
+}
+function extractDateFromFolder($folderName)
+{
+ 
+    $dateTokens = $folderName -split "_"
+ 
+                if ($dateTokens.Count -ne 3)
+                {
+                                return $null
+                }
+               
+                $dateString = ($dateTokens[0] + "/" + $dateTokens[1] + "/" + $dateTokens[2])
+               
+                $dateObject = [dateTime]::ParseExact($dateString, "MM/dd/yyyy" , $null)
+               
+                # Write-Host $dateObject
+ 
+                return $dateObject
+}
+function extractDate($path)
+{
+ 
+  $pathTokens = $path -split "-"
+ 
+  $dateTokens = $pathTokens[1] -split "_"
+  $timeTokens = $pathTokens[2] -split "_"
+ 
+  $timeString = ($dateTokens[0] + "/" + $dateTokens[1] + "/" + $dateTokens[2] + " " + $timeTokens[0] + ":" + $timeTokens[1] + ":" + $timeTokens[2])
+ 
+  $dateObject = [dateTime]::ParseExact($timeString, "MM/dd/yyyy HH:mm:ss" , $null)
+ 
+  return $dateObject
+}
+ 
 # Main program
-
-$pathToFolder = "$env:userprofile\Desktop\testDirectory\"
-
-# Write-Host $pathToFolder
-
-$dates = getDates
-
-updateFilesInRange $dates $pathToFolder
+ 
+updateFilesTest
